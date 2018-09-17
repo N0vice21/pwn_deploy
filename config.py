a406f01d92218647469529cc8ff72c3b86a0fcfb
giantbranch <giantbranch@qq.com>
2018-09-17 21:37:00 +0800

    update config.py

--- config.py
+++ config.py
@@ -32,8 +32,8 @@ XINETD = '''service ctf
 
 DOCKERFILE = '''FROM ubuntu:16.04
 
-RUN sed -i 's/archive.ubuntu.com/asia-east1.gce.archive.ubuntu.com/g' /etc/apt/sources.list && apt update && apt-get install -y lib32z1 xinetd
-#apt update && apt-get install -y lib32z1 xinetd
+RUN sed -i 's/archive.ubuntu.com/asia-east1.gce.archive.ubuntu.com/g' /etc/apt/sources.list && apt update && apt-get install -y lib32z1 xinetd && rm -rf /var/lib/apt/lists/ && rm -rf /root/.cache && apt-get autoclean && rm -rf /tmp/* /var/lib/apt/* /var/cache/* /var/log/*
+#apt update && apt-get install -y lib32z1 xinetd && rm -rf /var/lib/apt/lists/ && rm -rf /root/.cache && apt-get autoclean && rm -rf /tmp/* /var/lib/apt/* /var/cache/* /var/log/*
 
 COPY ./'''+ XINETD_CONF_FILENAME +''' /etc/xinetd.d/pwn
 
